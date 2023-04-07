818a6cf6f0510b832b0d33f19759dbe074421cc4
Ravil Galeyev <dehasi@yandex.ru>
2023-04-07 22:51:00 +0200

    Run kafka storage, use kafka as service

--- scripts/4-sync-files.sh
+++ scripts/4-sync-files.sh
@@ -5,7 +5,7 @@ chmod 666 /opt/kafka/config/kraft/server.properties
 
 mv /tmp/kafka /etc/init.d/kafka
 chmod 777 /etc/init.d/kafka
-
+chown root:root /etc/init.d/kafka
 
 mv /tmp/start-kafka.sh /home/vagrant/start-kafka.sh
 chmod 777 /home/vagrant/start-kafka.sh
