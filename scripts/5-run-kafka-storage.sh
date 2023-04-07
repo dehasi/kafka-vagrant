35e7d6b09dedc84f5b91f970ac327a9fdd5734a1
Ravil Galeyev <dehasi@yandex.ru>
2023-04-07 22:46:56 +0200

    Split running storage and kafka

--- scripts/5-run-kafka-storage.sh
+++ scripts/5-run-kafka-storage.sh
@@ -4,10 +4,10 @@ sudo su vagrant && cd ~
 echo $(pwd)
 
 PATH="$PATH:/opt/kafka/bin"
-echo 'export PATH="$PATH:/opt/kafka/bin"' >> /etc/profile
+sudo echo 'export PATH="$PATH:/opt/kafka/bin"' >> /etc/profile
 
 KAFKA_CLUSTER_ID="$(/opt/kafka/bin/kafka-storage.sh random-uuid)"
 
-echo "export KAFKA_CLUSTER_ID=${KAFKA_CLUSTER_ID}" >> /etc/profile
+sudo echo "export KAFKA_CLUSTER_ID=${KAFKA_CLUSTER_ID}" >> /etc/profile
 
 kafka-storage.sh format -t ${KAFKA_CLUSTER_ID} -c /opt/kafka/config/kraft/server.properties
